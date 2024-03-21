05a6ff393a795617e2cf7da31210b143349e99df
velikarade <veli.kara@docc.techstarter.de>
2024-03-21 18:00:39 +0100

    Button is added

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,14 @@
-Array.from(document.getElementsByTagName('input')).forEach
-    (element => {
-        element.addEventListener('change', updateUI);
-    });
+//Array.from(document.getElementsByTagName('input')).forEach
+//   (element => {
+//        element.addEventListener('change', updateUI);
+//    });
+
+
+const berechnungButton = document.getElementById('berechnung');
+berechnungButton.addEventListener('click', function (){
+    updateUI()
+})
+
 function updateUI() {
     // Logik zur Aktualisierung des UI hier
     const weight = document.getElementById('weight').value;
@@ -9,14 +16,18 @@ function updateUI() {
     const bmi = calculateBMI(weight, height);
     console.log("weight: %s, height: %s, bmi: %s", weight,
         height, bmi);
+
+        document.getElementById('result').innerHTML = "Das Ergebnis: " + bmi;
+    
 }
 function calculateBMI(weight, height) {
     // Logik zur Berechnung des BMI hier
-
     const heightInMeters = height / 100; // Umrechnung von Zentimetern in Meter
-
     const bmi = Math.round(weight / (heightInMeters * heightInMeters));
-
     return bmi;
 
 }
+
+//function zeigt (){
+//    document.getElementById('result').innerHTML = "Das Ergebnis: " + bmi;
+//}
